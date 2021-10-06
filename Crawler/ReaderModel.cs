37a1e0c7dc6722d3af4dc16adf9508c6639cfba9
LAPTOP-0HSO4OG9\ngonh <ngonhatduy240297@gmail.com>
2021-10-06 13:20:57 +0700

    test

---
+++ Crawler/ReaderModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crawler
+{
+    public class ReaderModel
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+    }
+}
